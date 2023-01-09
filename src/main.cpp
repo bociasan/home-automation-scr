1d8cd616b972a2fda9958ef41da84118d4ab4d57
AzureAD\AlexandruBoiciuc <alex.boiciuc@beamlogic.com>
2023-01-09 16:25:08 +0200

    Added state.

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,24 @@
 #include <Servo.h>
 
 #define DHTTYPE DHT11
-
-#define pinServo 2
-#define pinDHT 5
-#define pinGas 0
+#define pinServo D4
+#define pinDHT D1
+#define pinGas D3
 #define pinLEDout D7 // 13
 #define pinLEDin D6  // 12
 #define pinLUMEN D5  // 4
 
-bool lightAutomation = true;
-bool ledOutVal = false;
-bool ledInVal = false;
-bool isLocked = true;
-
-float temperatureValue = 0.0;
-float humidityValue = 0.0;
-bool gasValue = false;
-bool isNIGHT = false;
-// int pos = 0;
+struct
+{
+  float temperatureValue = 0.0;
+  float humidityValue = 0.0;
+  bool gasValue = false;
+  bool isNIGHT = false;
+  bool lightAutomation = true;
+  bool ledOutVal = false;
+  bool ledInVal = false;
+  bool isLocked = true;
+} currentState, previousState;
 
 Servo myservo;
 DHT dht(pinDHT, DHTTYPE);
@@ -29,14 +29,14 @@ void openDoor()
 {
   myservo.write(180);
   Serial.println("Open door.");
-  isLocked = false;
+  currentState.isLocked = false;
 }
 
 void closeDoor()
 {
   myservo.write(0);
   Serial.println("Clsoe door.");
-  isLocked = true;
+  currentState.isLocked = true;
 }
 
 int readGasSensor()
@@ -52,8 +52,6 @@ int readGasSensor()
 }
 float readTemperatureSensor()
 {
-  float t = 0.0;
-
   float newT = dht.readTemperature();
   if (isnan(newT))
   {
@@ -62,55 +60,54 @@ float readTemperatureSensor()
   }
   else
   {
-    t = newT;
-    Serial.println(t);
-    return t;
+    return newT;
   }
 }
+
 float readHumiditySensor()
 {
-  float h = 0.0;
-
   float newH = dht.readHumidity();
   if (isnan(newH))
   {
-    if (isnan(newH))
-    {
-      Serial.println("Failed to read from DHT sensor!");
-      return -1;
-    }
-    else
-    {
-      h = newH;
-      Serial.println(h);
-      return h;
-    }
+    Serial.println("Failed to read from DHT sensor!");
+    return -1;
+  }
+  else
+  {
+    return newH;
   }
   return -1;
 }
+
 void turnLEDinON()
 {
   digitalWrite(pinLEDin, HIGH);
+  currentState.ledInVal = true;
 }
+
 void turnLEDinOFF()
 {
   digitalWrite(pinLEDin, LOW);
+  currentState.ledInVal = false;
 }
+
 void turnLEDoutON()
 {
   digitalWrite(pinLEDout, HIGH);
+  currentState.ledOutVal = true;
 }
+
 void turnLEDoutOFF()
 {
   digitalWrite(pinLEDout, LOW);
+  currentState.ledOutVal = false;
 }
+
 void LEDup()
 {
-  if (lightAutomation) // Light Automation ON
+  if (currentState.lightAutomation) // Light Automation ON
   {
-    isNIGHT = digitalRead(pinLUMEN);
-
-    if (isNIGHT)
+    if (currentState.isNIGHT)
     {
       turnLEDoutON();
       turnLEDinON();
@@ -145,6 +142,7 @@ void LEDup()
 
 void setup()
 {
+  Serial.begin(115200);
   pinMode(pinLUMEN, INPUT);   // LUMEN pin as input.
   pinMode(pinLEDin, OUTPUT);  // LED pin as output.
   pinMode(pinLEDout, OUTPUT); // LED pin as output.
@@ -153,18 +151,41 @@ void setup()
   dht.begin();
 }
 
+void readSensors()
+{
+  currentState.isNIGHT = digitalRead(pinLUMEN);
+  currentState.temperatureValue = readTemperatureSensor();
+  currentState.humidityValue = readHumiditySensor();
+  currentState.gasValue = readGasSensor();
+  // currentState.lightAutomation = true;
+  // currentState.ledOutVal = false;
+  // currentState.ledInVal = false;
+  // currentState.isLocked = true;
+}
+
+void algorithms()
+{
+  LEDup();
+}
+
 void loop()
 {
-  Serial.print(temperatureValue);
-  Serial.print("    ");
-  Serial.print(humidityValue);
-  Serial.print("    ");
-  Serial.print(gasValue);
-  Serial.print("    ");
-  Serial.print(ledInVal);
-  Serial.print("    ");
-  Serial.print(ledOutVal);
-  Serial.print("    ");
-  Serial.print(lightAutomation);
-  Serial.print("    ");
+  readSensors();
+  algorithms();
+
+  Serial.printf("T=%f | H=%f | Gas=%d | Night=%d \n", currentState.temperatureValue, currentState.humidityValue, currentState.gasValue, currentState.isNIGHT);
+
+  // Serial.print(currentState.temperatureValue);
+  // Serial.print("    ");
+  // Serial.print(currentState.humidityValue);
+  // Serial.print("    ");
+  // Serial.print(currentState.gasValue);
+  // Serial.print("    ");
+  // Serial.print(currentState.ledInVal);
+  // Serial.print("    ");
+  // Serial.print(currentState.ledOutVal);
+  // Serial.print("    ");
+  // Serial.print(currentState.lightAutomation);
+  // Serial.print("    ");
+  // Serial.println();
 }
